2359083079274ac92906495db3c98037535a8972
jakkan <>
2023-01-03 21:31:39 +0100

    Remove settings that are already set by lazy.

--- init.lua
+++ init.lua
@@ -1,7 +1,3 @@
--- disable netrw at the very start of your init.lua (strongly advised)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 require('core.options')
 require('core.lazy')
 require('core.keymappings')
