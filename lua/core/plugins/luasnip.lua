7dca653921379fef2dcb087dcb9a74b11c9bd971
jakkan <>
2023-01-05 23:33:55 +0100

    Use less magic when setting user defined snippets

--- lua/core/plugins/luasnip.lua
+++ lua/core/plugins/luasnip.lua
@@ -13,12 +13,25 @@ local M = {
 function M.config()
   local luasnip = require("luasnip")
 
+  luasnip.add_snippets(nil, {
+    markdown = {
+        luasnip.snippet({
+            trig = "title",
+            namr = "Title",
+            dscr = "Insert filename as title",
+        }, {
+            luasnip.function_node(function() return {vim.fn.expand("%:t:r")} end, {}),
+        }),
+    },
+
+  })
   luasnip.config.setup({
     history = true,
     enable_autosnippets = true,
     -- Update more often, :h events for more info.
     -- updateevents = "TextChanged,TextChangedI",
   })
+
 end
 
 return M
--- snippets/markdown.lua
+++
@@ -1,8 +0,0 @@
--- No useful snippets yet, but hope to add some in the future
--- As defining all of the snippet-constructors (s, c, t, ...) in every file is rather cumbersome, luasnip will bring some globals into scope for executing these files. defined by snip_env in setup
-require("luasnip.loaders.from_lua").lazy_load()
-local env = snip_env
-
-return {
-  env.s("date", env.p(os.date, "%Y-%m-%d")),
-}
