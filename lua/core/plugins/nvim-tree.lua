8a59fbc4b65d0fcd811c0ce62e7dcf91a8e9b597
jakkan <>
2023-01-10 23:02:58 +0100

    Add search in path functionality

--- lua/core/plugins/nvim-tree.lua
+++ lua/core/plugins/nvim-tree.lua
@@ -4,8 +4,22 @@ local M = {
   'nvim-tree/nvim-tree.lua',
 }
 
+local function custom_callback(callback_name)
+    return string.format(":lua require('core.plugins.nvim-tree.tree-utils').%s()<CR>", callback_name)
+end
+
 function M.config()
-  require("nvim-tree").setup()
+  require("nvim-tree").setup({
+    view = {
+        mappings = {
+            list = {
+              -- search in path        
+              { key = "<c-f>", cb = custom_callback "launch_find_files" },
+              { key = "<c-g>", cb = custom_callback "launch_live_grep" },
+            }
+        }
+    }
+  })
 end
 
 return M
